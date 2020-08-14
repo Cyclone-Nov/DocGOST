5dc68a8b9395dc697fcb1b9b88fc19347358ebf0
arfus <arfus@yandex.ru>
2020-08-14 16:42:40 +0300

    add some defines

--- Common/Defines.cs
+++ Common/Defines.cs
@@ -79,10 +79,112 @@ namespace GostDOC.Common
             return res;
         }
 
+        /// <summary>
+        /// Получить строку с кодом документа по типу документа
+        /// </summary>
+        /// <param name="aDocType">Type of a document.</param>
+        /// <returns></returns>
+        public static string GetDocumentCode(DocType aDocType)
+        {            
+            switch(aDocType)
+            {
+                case DocType.Bill:
+                    return @"ВП";
+                case DocType.ItemsList:
+                    return @"ПЭ3";
+                case DocType.Specification:
+                case DocType.D27:
+                case DocType.None:
+                    return string.Empty;
+            }
+            return string.Empty;
+        }
+
     }
 
     public static class Constants
     {
+        /// <summary>
+        /// индекс основного исполнения (конфигурации) в виде строки
+        /// </summary>
+        public static readonly string MAIN_CONFIG_INDEX = "-00";
+
+        /// <summary>
+        /// название графы №1 (Наименование изделия) основной надписи в структуре xml
+        /// </summary>
+        public static readonly string GRAPH_1 = "Наименование";
+        /// <summary>
+        /// название графы №2 Обозначение документа по ГОСТ 2.201 в структуре xml
+        /// </summary>
+        public static readonly string GRAPH_2 = "Обозначение";
+        /// <summary>
+        /// название графы №4 Обозначение Литеры А изделия в структуре xml
+        /// </summary>
+        public static readonly string GRAPH_4 = "Литера";
+        /// название графы №4а Обозначение Литеры О изделия в структуре xml
+        /// </summary>
+        public static readonly string GRAPH_4a = "Литера2";
+        /// название графы №4б Обозначение Литеры О1 изделия в структуре xml
+        /// </summary>
+        public static readonly string GRAPH_4b = "Литера3";
+        /// <summary>
+        /// название графы №9 Наименование организации в структуре xml
+        /// </summary>
+        public static readonly string GRAPH_9 = "Организация";
+        /// <summary>
+        /// название графы №10 Должность контролирующего в структуре xml
+        /// </summary>
+        public static readonly string GRAPH_10 = "Дополнительная графа";
+        /// <summary>
+        /// название графы №11 Фамилия разработчика для спецификации в структуре xml
+        /// </summary>
+        public static readonly string GRAPH_11sp_dev = "Разработал конструктор";
+        /// <summary>
+        /// название графы №11 Фамилия разработчика для ВП в структуре xml
+        /// </summary>
+        public static readonly string GRAPH_11bl_dev = "Разработал схемотехник";
+        /// <summary>
+        /// название графы №11 Фамилия проверяющего для спецификации в структуре xml
+        /// </summary>
+        public static readonly string GRAPH_11sp_chk = "Проверил конструктор";
+        /// <summary>
+        /// название графы №11 Фамилия разработчика для ВП в структуре xml
+        /// </summary>
+        public static readonly string GRAPH_11bl_chk = "Проверил схемотехник";
+        /// <summary>
+        /// название графы №11 Фамилия нормоконтролера в структуре xml
+        /// </summary>
+        public static readonly string GRAPH_11norm = "Нормоконтроль";
+        /// <summary>
+        /// название графы №11 Фамилия утверждающего в структуре xml
+        /// </summary>
+        public static readonly string GRAPH_11affirm = "Утвердил";
+        /// <summary>
+        /// название графы №11 Фамилия утверждающего в структуре xml
+        /// </summary>
+        public static readonly string GRAPH_11app = "Дополнительная графа фамилия";
+        /// <summary>
+        /// название графы №14 Порядковый номер изменения в документе в структуре xml
+        /// </summary>
+        public static readonly string GRAPH_14 = "Порядковый номер изменения";
+        /// <summary>
+        /// название графы №15
+        /// </summary>
+        public static readonly string GRAPH_15 = "";
+        /// <summary>
+        /// название графы №16 Номер документа изменение в структуре xml
+        /// </summary>
+        public static readonly string GRAPH_16 = "Номер документа изменение";
+        /// <summary>
+        /// название графы №18 Дата изменения документа в структуре xml
+        /// </summary>
+        public static readonly string GRAPH_18 = "Дата изменения";
+        /// <summary>
+        /// название графы №25 Первичное применение изделия в структуре xml
+        /// </summary>
+        public static readonly string GRAPH_25 = "Перв. примен";
+        
+
         public static readonly string DefaultGroupName = "Без группы";
         public static readonly string GroupNameDoc = "Документация";
         public static readonly string GroupNameSp = "Раздел СП";
