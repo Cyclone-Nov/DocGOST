5ac7d80239cb4f31f79c9e82343119b70fb5058a
arfus <arfus@yandex.ru>
2020-09-11 11:43:01 +0300

    call PDF prepare and data prepare separately

--- Models/DocManager.cs
+++ Models/DocManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using System.Collections.Generic;
 using System.Linq;
 using GostDOC.Common;
@@ -19,6 +20,8 @@ namespace GostDOC.Models
 
         private PdfManager _pdfManager { get; } = PdfManager.Instance;
 
+        private PrepareManager _prepareDataManager { get; } = PrepareManager.Instance;
+
         public Project Project { get; private set; } = new Project();
 
         #region Public
@@ -33,22 +36,40 @@ namespace GostDOC.Models
             return XmlManager.SaveData(Project, aFilePath);
         }
 
+        /// <summary>
+        /// подготовить данных перед экспортом
+        /// </summary>
+        /// <param name="aDocType">Тип документа</param>
+        /// <returns></returns>        
+        public bool PrepareData(DocType aDocType)
+        {
+            return _prepareDataManager.PrepareDataTable(aDocType, Project.Configurations);
+        }
 
         /// <summary>
         /// сохранить изменения в pdf для типа документа aDocType
         /// </summary>
         /// <param name="aDocType">Тип документа</param>
-        /// <returns></returns>
-        /// <exception cref="NotImplementedException"></exception>
-        public bool SaveChangesInPdf(DocType aDocType)
+        /// <returns></returns>        
+        public bool PreparePDF(DocType aDocType)
         {
-            return _pdfManager.SaveChanges(aDocType, Project);            
+            DataTable dataTable = _prepareDataManager.GetDataTable(aDocType);
+            IDictionary<string, string> mainConfigGraphs = null;
+            if (GetMainConfigurationGraphs(out mainConfigGraphs))
+            {
+                return _pdfManager.PreparePDF(aDocType, dataTable, mainConfigGraphs);
+            }            
+            return false;
         }
 
-
+        /// <summary>
+        /// Получить данные для PDF
+        /// </summary>
+        /// <param name="aDocType">Type of a document.</param>
+        /// <returns></returns>
         public byte[] GetPdfData(DocType aDocType)
         {
-            return _pdfManager.GetPDFData(aDocType);            
+            return _pdfManager.GetPDFData(aDocType);
         }
 
         /// <summary>
@@ -59,9 +80,26 @@ namespace GostDOC.Models
         /// <exception cref="NotImplementedException"></exception>
         public string GetPdfFileName(DocType aDocType)
         {
-            return _pdfManager.GetFileName(aDocType);            
+            return _pdfManager.GetFileName(aDocType);
         }
 
         #endregion Public
+
+        /// <summary>
+        /// Получить графы для основной конфигурации
+        /// </summary>        
+        /// <param name="mainGraphs">словарь графов основного исполнения</param>
+        /// <returns>true - словарь графов извлечен успешно</returns>
+        private bool GetMainConfigurationGraphs(out IDictionary<string, string> mainGraphs)
+        {
+            mainGraphs = null;
+            Configuration mainConfig = null;
+            if (Project.Configurations.TryGetValue(Constants.MAIN_CONFIG_INDEX, out mainConfig))
+            {
+                mainGraphs = mainConfig.Graphs;
+                return true;
+            }
+            return false;
+        }
     }
 }
