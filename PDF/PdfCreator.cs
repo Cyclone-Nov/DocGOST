fe97453b3a55b9af09ed0af34b607b636e07dbdb
arfus <arfus@yandex.ru>
2020-11-19 15:55:17 +0300

    fixes

--- PDF/PdfCreator.cs
+++ PDF/PdfCreator.cs
@@ -541,7 +541,8 @@ namespace GostDOC.PDF
             mainTable.AddCell(fullyBorderCell.Clone(false).Add(CreatePaddingTopParagraph(GetGraph(Constants.GRAPH_4))));
             mainTable.AddCell(fullyBorderCell.Clone(false).Add(CreatePaddingTopParagraph(GetGraph(Constants.GRAPH_4a))));
             mainTable.AddCell(fullyBorderCell.Clone(false).Add(CreatePaddingTopParagraph(GetGraph(Constants.GRAPH_4b))));
-            mainTable.AddCell(fullyBorderCell.Clone(false).Add(CreatePaddingTopParagraph(titleBlockStruct.CurrentPage.ToString())));
+            string currPage = (titleBlockStruct.Pages == 1) ? string.Empty : titleBlockStruct.CurrentPage.ToString();
+            mainTable.AddCell(fullyBorderCell.Clone(false).Add(CreatePaddingTopParagraph(currPage)));
             mainTable.AddCell(fullyBorderCell.Clone(false).Add(CreatePaddingTopParagraph(titleBlockStruct.Pages.ToString())));
             
             #endregion
