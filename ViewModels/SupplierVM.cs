b13fb1956c16eb2cf5af35d0707111429796099d
arfus <arfus@yandex.ru>
2020-11-26 11:28:39 +0300

    add models for D27 bill

---
+++ ViewModels/SupplierVM.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GostDOC.Common;
+
+namespace GostDOC.ViewModels
+{
+    class SupplierVM
+    {
+        /// <summary>
+        /// имя поставщика
+        /// </summary>
+        /// <value>
+        /// The supplier.
+        /// </value>
+        public ObservableProperty<string> Name { get; set; } = new ObservableProperty<string>();
+
+        /// <summary>
+        /// количество
+        /// </summary>
+        /// <value>
+        /// The quantity.
+        /// </value>
+        public ObservableProperty<int> Quantity { get; set; } = new ObservableProperty<int>();
+
+        /// <summary>
+        /// цена, руб
+        /// </summary>
+        /// <value>
+        /// The price.
+        /// </value>
+        public ObservableProperty<float> Price { get; set; } = new ObservableProperty<float>();
+
+        /// <summary>
+        /// Тип НДС
+        /// </summary>
+        /// <value>
+        /// The type of the tax.
+        /// </value>
+        public ObservableProperty<TaxTypes> TaxType { get; set; } = new ObservableProperty<TaxTypes>();
+                
+        /// <summary>
+        /// Итоговая стоимость с учетом НДС
+        /// </summary>
+        /// <value>
+        /// The price with tax.
+        /// </value>
+        public ObservableProperty<float> PriceWithTax { get; set; } = new ObservableProperty<float>(0);
+
+        public SupplierVM(string aName, int aQuantity, float aPrice, TaxTypes aTax)
+        {
+            Name.Value = aName;
+            Quantity.Value = aQuantity;
+            Price.Value = aPrice;
+            TaxType.Value = aTax;
+            PriceWithTax.Value = Common.Converters.GetPriceWithTax(aPrice, aTax);
+
+        }
+    }
+}
---
+++ ViewModels/WarehouseDeliveryVM.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GostDOC.ViewModels
+{
+    class WarehouseDeliveryVM
+    {
+        /// <summary>
+        /// дата выачи со склада
+        /// </summary>
+        /// <value>
+        /// The delivery date.
+        /// </value>
+        public ObservableProperty<string> DeliveryDate { get; } = new ObservableProperty<string>();
+
+        /// <summary>
+        /// количество поступившее на склад
+        /// </summary>
+        /// <value>
+        /// The quantity.
+        /// </value>
+        public ObservableProperty<int> Quantity { get; } = new ObservableProperty<int>();
+
+        /// <summary>
+        /// кому выдано
+        /// </summary>
+        /// <value>
+        /// 
+        /// </value>
+        public ObservableProperty<string> WhomWereIssued { get; } = new ObservableProperty<string>();
+
+        public WarehouseDeliveryVM(string aDate, int aQuantity, string WhomIssued)
+        {
+            DeliveryDate.Value = aDate;
+            Quantity.Value = aQuantity;
+            WhomWereIssued.Value = WhomIssued;
+        }
+    }
+}
