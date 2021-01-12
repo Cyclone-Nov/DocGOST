9132d3985118fa18eaaefdc87475a1c52ed51611
arfus <arfus@yandex.ru>
2021-01-12 17:02:46 +0300

    add comments

--- DataPreparation/BasePreparer.cs
+++ DataPreparation/BasePreparer.cs
@@ -35,21 +35,21 @@ public abstract class BasePreparer {
     /// <summary>
     /// формирование таблицы данных
     /// </summary>
-    /// <param name="aConfigs">a configs.</param>
+    /// <param name="aConfigs">словарь конфигураций типа IDictionary<string ConfigName, Configuration Config>, где ConfigName - имя конфигурации, Config - конфигурация</param>
     /// <returns></returns>
     public abstract DataTable CreateDataTable(IDictionary<string, Configuration> aConfigs);
 
     /// <summary>
-    /// Gets the document sign.
+    /// Получить обозначение документа
     /// </summary>
-    /// <param name="aMainConfig">a main configuration.</param>
+    /// <param name="aMainConfig">основная конфигурация (-00)</param>
     /// <returns></returns>
     public abstract string GetDocSign(Configuration aMainConfig);
 
     /// <summary>
     /// создать таблицу данных
     /// </summary>
-    /// <param name="aDataTableName">Name of a data table.</param>
+    /// <param name="aDataTableName">имя таблицы данных</param>
     /// <returns></returns>
     protected abstract DataTable CreateTable(string aDataTableName);
 
@@ -71,9 +71,7 @@ public abstract class BasePreparer {
                     //var shemas = docсomponents.Where(val => (
                     //    val.GetProperty(Constants.ComponentDocCode).EndsWith("С3") ||
                     //    val.GetProperty(Constants.ComponentDocCode).EndsWith("Э3"))
-                    //);
-
-                    
+                    //);                    
                     // в любом случае берем первую
                     if (docсomponents.Count() > 0) {
                         designation = docсomponents.First().GetProperty(Constants.ComponentSign);
@@ -102,7 +100,8 @@ public abstract class BasePreparer {
     /// <param name="aConfigs">список всез конфигураций</param>
     /// <returns>список словарей элементов</returns>
     protected IEnumerable<Dictionary<string, Component>>
-    MakeComponentDesignatorsDictionaryOtherConfigs(IDictionary<string, Configuration> aConfigs) {
+    MakeComponentDesignatorsDictionaryOtherConfigs(IDictionary<string, Configuration> aConfigs)
+    {
         var result = new List<Dictionary<string, Component>>();
         // ваыбираем все конфигурации кроме базовой
         var configs = aConfigs.Where(val => !string.Equals(val.Key, Constants.MAIN_CONFIG_INDEX));
