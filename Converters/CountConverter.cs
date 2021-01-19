24f2034271a49dc9f387db2fde346a0394ebad91
arfus <arfus@yandex.ru>
2021-01-19 15:49:21 +0300

    fix crash

--- Converters/CountConverter.cs
+++ Converters/CountConverter.cs
@@ -8,14 +8,23 @@ using System.Windows.Data;
 
 namespace GostDOC.Converters
 {
-    class CountConverter : IValueConverter
+    public class CountConverter : IValueConverter
     {
         object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value == null)
                 return "";
 
-            int val = Convert.ToInt32(value);
+            int val = 0;
+            try
+            {
+                val = Convert.ToInt32(value);
+            }
+            catch
+            {
+                val = 0;
+            }
+
             if (val == 0)
             {
                 return "";
