57ed7cf2968624b260951922e3b3efdd29414950
arfus <arfus@yandex.ru>
2021-02-12 10:52:40 +0300

    add comments to PdfUtils

--- DocGOSTSources/PDF/PdfUtils.cs
+++ DocGOSTSources/PDF/PdfUtils.cs
@@ -9,26 +9,36 @@ using GostDOC.Common;
 namespace GostDOC.PDF
 {
 
+    /// <summary>
+    /// Расшириение для класса String
+    /// </summary>
     public static class StringExt
     {
+        /// <summary>
+        /// обрезать строку value 
+        /// </summary>
+        /// <param name="value">исходная строка</param>
+        /// <param name="maxLength">максимальное количество символов в строке</param>
+        /// <returns>результат</returns>
         public static string Truncate(this string value, int maxLength)
         {
-            if (string.IsNullOrEmpty(value)) return value;
+            if (string.IsNullOrEmpty(value))
+                return value;
             return value.Length <= maxLength ? value : value.Substring(0, maxLength);
         }
     }
 
     /// <summary>
-    /// 
+    /// класс с вспомогательными функциями для создания PDF
     /// </summary>
     public static class PdfUtils
     {
         /// <summary>
-        /// Gets the count page.
+        /// получить количество страниц в документе в зависимости от типа документа
         /// </summary>
-        /// <param name="aType">a type.</param>
-        /// <param name="">The .</param>
-        /// <returns></returns>
+        /// <param name="aType">тип документа.</param>
+        /// <param name="">количество строк в документе</param>
+        /// <returns>количество страниц</returns>
         public static int GetCountPage(DocType aType, int aTotalRows)
         {
             int RowsOnFirstPage;
@@ -82,11 +92,14 @@ namespace GostDOC.PDF
 
         /// <summary>
         /// Разбить строку на несколько строк исходя из длины текста
+        /// Если задано несколько разделителей, то приоритетным считается пробел
         /// </summary>
         /// <param name="aLength">максимальная длина в мм</param>
         /// <param name="aString">строка для разбивки</param>
-        /// <param name="aFontSize">размер шрифта</param>
-        /// <returns></returns>
+        /// <param name="aDelimiters">массив символов-разделителей, по которым можно разбивать строку</param>
+        /// <param name="aFontSize">размер шрифта, по умолчанию PdfDefines.DefaultFontSize</param>
+        /// <param name="aUseGOST">признак, что надо разбивать относительно массива специальных подстрок</param>
+        /// <returns>список строк, являющихся разбивкой исходной строки aString</returns>
         public static List<string> SplitStringByWidth(float aLength, string aString, char[] aDelimiters, float aFontSize = PdfDefines.DefaultFontSize, bool aUseGOST = false)
         {
             if (string.IsNullOrEmpty(aString) || aDelimiters == null || aDelimiters.Length == 0)
