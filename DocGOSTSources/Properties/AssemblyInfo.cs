0f9735e359e60145f17821d0a83e41ec2c6e091d
arfus <arfus@yandex.ru>
2021-02-17 12:32:18 +0300

    update version to 1.1.1.30

--- DocGOSTSources/Properties/AssemblyInfo.cs
+++ DocGOSTSources/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GostDOC")]
+[assembly: AssemblyTitle("GOSTDoc")]
 [assembly: AssemblyDescription("Creates design documantation by standard")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Central Research Institute Cyclone")]
-[assembly: AssemblyProduct("GostDOC")]
+[assembly: AssemblyProduct("GOSTDoc")]
 [assembly: AssemblyCopyright("JSC CRI Cyclone ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.1.29")]
-[assembly: AssemblyFileVersion("1.1.1.29")]
+[assembly: AssemblyVersion("1.1.1.30")]
+[assembly: AssemblyFileVersion("1.1.1.30")]
